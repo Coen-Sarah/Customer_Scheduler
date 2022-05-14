359c29e04398422ab06953bedbf1c89cb33a0767
Coen-Sarah <sarah.e.coen.07@gmail.com>
2022-05-14 18:57:18 -0500

    Tester code for Timezone support added

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.sql.ResultSet;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static DAO.AppointmentDAO.getAllAppointments;
 import static DAO.AppointmentDAO.getAppointment;
@@ -19,6 +23,7 @@ import static DAO.Query.getResult;
 import static DAO.Query.makeQuery;
 import static DAO.UserDAO.getAllUsers;
 import static DAO.UserDAO.getUser;
+import static java.time.ZoneId.getAvailableZoneIds;
 
 public class Main extends Application {
 
@@ -34,11 +39,36 @@ public class Main extends Application {
     public static void main(String[] args) {
         try {
             makeConnection();
+            String format = "M / d / y  hh:mm a ";
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
 
+            LocalDateTime testTime = getAppointment(1).getStartTime();
+            ZonedDateTime zoneTestTime = testTime.atZone(ZoneId.of("UTC"));
+            ZonedDateTime myTestTime = zoneTestTime.withZoneSameInstant(ZoneId.systemDefault());
+            ZonedDateTime easternTime = zoneTestTime.withZoneSameInstant(ZoneId.of("America/New_York"));
 
-            System.out.println(getAppointment(1));
+            System.out.println(getAvailableZoneIds());
+
+            String zoneTimeFormatted = testTime.format(formatter);
+
+            format ="M/d/y hh:mm a  ZZZZ";
+            formatter = DateTimeFormatter.ofPattern(format);
+
+            String testTimeFormatted = zoneTestTime.format(formatter);
+            String myTimeFormatted = myTestTime.format(formatter);
+            String easterTimeFormatted = easternTime.format(formatter);
+
+            System.out.println(testTime);
+            System.out.println(testTimeFormatted + "\n");
+            System.out.println(zoneTestTime);
+            System.out.println(zoneTimeFormatted + "\n");
+            System.out.println(myTestTime);
+            System.out.println(myTimeFormatted + "\n");
+            System.out.println(easternTime);
+            System.out.println(easterTimeFormatted);
+
         }catch(Exception ex){
-
+            ex.printStackTrace();
         }
 
         //launch(args);
