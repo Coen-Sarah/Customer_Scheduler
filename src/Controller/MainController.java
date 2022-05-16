c1b0746c79342c3ceb4da56f4e7f8be7d03c38c5
Coen-Sarah <sarah.e.coen.07@gmail.com>
2022-05-16 11:21:33 -0700

    Implemented clock that adjusts to timezone and fr and en language bundles

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,4 +1,53 @@
 package Controller;
 
-public class MainController {
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.value.ObservableStringValue;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    public Label dateLabel;
+    public Label timeLabel;
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public Button loginButton;
+
+    public ResourceBundle rb;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Locale.setDefault(new Locale("en"));
+        rb = ResourceBundle.getBundle("C195", Locale.getDefault());
+
+    //TODO CITE: https://stackoverflow.com/questions/42383857/javafx-live-time-and-date
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e ->
+                timeLabel.setText(ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm zz")))
+        ),
+                new KeyFrame(Duration.seconds(1))
+        );
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+
+
+    }
+
+    public void login(){
+        //System.out.println(timeLabel.getText().toString());
+
+        System.out.println(rb.getString("hello"));
+        System.out.println();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,30 @@
-import Model.User;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.sql.ResultSet;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-import static DAO.AppointmentDAO.getAllAppointments;
 import static DAO.AppointmentDAO.getAppointment;
-import static DAO.ContactDAO.getAllContacts;
-import static DAO.ContactDAO.getContact;
-import static DAO.CustomerDAO.getAllCustomers;
 import static DAO.DBConnection.makeConnection;
-import static DAO.Query.getResult;
-import static DAO.Query.makeQuery;
-import static DAO.UserDAO.getAllUsers;
-import static DAO.UserDAO.getUser;
 import static java.time.ZoneId.getAvailableZoneIds;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/View/sample.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/View/main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
@@ -39,39 +32,15 @@ public class Main extends Application {
     public static void main(String[] args) {
         try {
             makeConnection();
-            String format = "M / d / y  hh:mm a ";
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
 
-            LocalDateTime testTime = getAppointment(1).getStartTime();
-            ZonedDateTime zoneTestTime = testTime.atZone(ZoneId.of("UTC"));
-            ZonedDateTime myTestTime = zoneTestTime.withZoneSameInstant(ZoneId.systemDefault());
-            ZonedDateTime easternTime = zoneTestTime.withZoneSameInstant(ZoneId.of("America/New_York"));
-
-            System.out.println(getAvailableZoneIds());
-
-            String zoneTimeFormatted = testTime.format(formatter);
-
-            format ="M/d/y hh:mm a  ZZZZ";
-            formatter = DateTimeFormatter.ofPattern(format);
-
-            String testTimeFormatted = zoneTestTime.format(formatter);
-            String myTimeFormatted = myTestTime.format(formatter);
-            String easterTimeFormatted = easternTime.format(formatter);
-
-            System.out.println(testTime);
-            System.out.println(testTimeFormatted + "\n");
-            System.out.println(zoneTestTime);
-            System.out.println(zoneTimeFormatted + "\n");
-            System.out.println(myTestTime);
-            System.out.println(myTimeFormatted + "\n");
-            System.out.println(easternTime);
-            System.out.println(easterTimeFormatted);
 
         }catch(Exception ex){
             ex.printStackTrace();
         }
 
-        //launch(args);
+        launch(args);
         Platform.exit();
     }
+
+
 }
